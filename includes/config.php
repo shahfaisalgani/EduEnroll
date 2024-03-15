a5c0278ce360525f88e963518c12be77c7249985
shahfaisalgani <54397227+shahfaisalgani@users.noreply.github.com>
2024-03-15 18:19:33 +0530

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -1,7 +1,7 @@
 <?php
-define('DB_SERVER','localhost');
-define('DB_USER','root');
-define('DB_PASS' ,'');
+define('DB_SERVER','ecommercedb000.mysql.database.azure.com');
+define('DB_USER',' "Faisalgani@ecommercedb000');
+define('DB_PASS' ,'09@Atik786');
 define('DB_NAME','onlinecourse');
 $con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
 // Check connection
